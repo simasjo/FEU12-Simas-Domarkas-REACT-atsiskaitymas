383104da6a09e3bbebb1562fa558210c801dcb64
simasjo <simas.domarkas@gmail.com>
2024-03-26 15:04:23 +0200

    Pridejau trynimo funkcijonaluma

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import { useContext } from "react";
+import UsersContext from "../../contexts/UsersContext";
+import CardsContext from "../../contexts/CardsContext";
+import { CardsActionTypes } from "../../contexts/CardsContext";
 
 const StyledDiv = styled.div`
    border: 1px solid black;
@@ -19,10 +23,25 @@ const StyledDiv = styled.div`
 `;
 
 const Card = ({ data }) => {
+
+   const { setCards } = useContext(CardsContext);
+   const { loggedInUser } = useContext(UsersContext);
+
     return ( 
         <StyledDiv>
         <h3>{data.title}</h3>
         <p>{data.description}</p>
+        {
+         loggedInUser.id === data.userId &&
+         <button
+         onClick={() => { 
+            setCards({
+               type: CardsActionTypes.delete,
+               id: data.id
+            })
+         }}
+        >Delete</button>
+        }
         </StyledDiv>
      );
 }
